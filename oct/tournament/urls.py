f050f0fd0c8cd33fb25e8d0b147db329247f6b02
HR Farmer <beatboxto3@gmail.com>
2023-06-13 19:53:13 -0500

    header

--- oct/tournament/urls.py
+++ oct/tournament/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("teams", views.teams, name="teams"),
+    path("mappools", views.mappools, name="mappools"),
+    path("bracket", views.bracket, name="bracket")
 ]
--- oct/tournament/views.py
+++ oct/tournament/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def index(req):
     return render(req, "tournament/index.html")
+
+
+def teams(req):
+    return render(req, "tournament/teams.html")
+
+def mappools(req):
+    return render(req, "tournament/mappools.html")
+
+def bracket(req):
+    return render(req, "tournament/bracket.html")
