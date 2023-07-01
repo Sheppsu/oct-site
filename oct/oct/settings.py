df806423a1b25402f2ede89760195331ba9c3b30
Sheepposu <apc2005osu@gmail.com>
2023-07-01 13:09:17 -0400

    fix MIME bug with css

--- oct/oct/settings.py
+++ oct/oct/settings.py
@@ -15,6 +15,9 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 from osu import AuthHandler, Scope, Client
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
